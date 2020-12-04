4b1ec5d8c6798f95bec506833a392a56dce91027
hecike3 <heczendorferpeter>
2020-12-04 18:05:52 +0100

    2020.12.04 óra végi anyag ( ͡° ͜ʖ ͡°)

--- librarian/issue_books.php
+++ librarian/issue_books.php
@@ -1,7 +1,7 @@
 <?php
  session_start();
  include "header.php";
- 
+ include "connection_lib.php";
  ?>
 
 <!-- page content area main -->
@@ -26,8 +26,19 @@
                             <tr>
                                 <td>
                                     <select class="form-control selectpicker">
-                                        <option >A</option>
-                                        <option >B</option>
+
+                                    <?php
+                                    $res=mysqli_query($link,"select enrollmentno from student_registration");
+                                    while($row=mysqli_fetch_array($res))
+                                    {
+                                        echo "<option>";
+                                        echo $row["enrollmentno"];
+                                        echo "</option>";
+                                    }
+                                    
+                                    
+                                    ?>
+
                                     </select>
                             
                                 </td>
@@ -36,6 +47,86 @@
                                 </td>
                             </tr>
                             </table>
+
+                            <?php
+                            if(isset($_POST["submit1"]))
+                            {
+                                ?>
+
+                                <table class="table table-bordered">
+
+                                    <tr>
+                                        <td>
+                                            <input type="text" class="form-control" name="enrollmentno" placeholder="enrollmentno" disabled>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>
+                                            <input type="text" class="form-control" name="studentname" placeholder="studentname" required>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>
+                                            <input type="text" class="form-control" name="studentsem" placeholder="studentsem" required>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>
+                                            <input type="text" class="form-control" name="studentcontact" placeholder="studentcontact" required>
+                                        </td>
+                                    </tr>
+                                    
+                                    <tr>
+                                        <td>
+                                            <input type="text" class="form-control" name="studentemail" placeholder="studentemail" required>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>
+                                            <select name="bookname" class="form-control selectpicker">
+                                                <?php
+                                                $res = mysqli_query($link,"select book_name from add_books");
+                                                    while($row=mysqli_fetch_array($res))
+                                                    {
+                                                        echo "<option>";
+                                                        echo $row["book_name"];
+                                                        echo "</option>";
+                                                    }
+                                                ?>
+                                            
+                                            </select>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>
+                                            <input type="text" class="form-control" name="bookissuedate" placeholder="bookissuedate" required>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>
+                                            <input type="text" class="form-control" name="studentusername" placeholder="studentusername" disabled>
+                                        </td>
+                                    </tr>
+                                    
+                                    <tr>
+                                        <td>
+                                            <input type="submit" class="form-control btn btn-default" name="submit2"  value="Felvétel">
+                                        </td>
+                                    </tr>
+                                
+                                </table>
+
+                                <?php
+                            }
+                            
+                            
+                            ?>
                                                 
                         </form>
 
