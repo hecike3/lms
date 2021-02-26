b60f21489f3b06c8086163eb052370cbdf584e35
hecike3 <heczendorferpeter@gmail.com>
2021-02-26 18:04:39 +0100

    2021.02.26

---
+++ assets/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dr.Code könyvtár</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark ">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">Dr.Code Könyvtár</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse right-align" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="index.php">Főoldal</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="konyvek.php">Könyvek</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="rolunk.php">Rólunk</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="kapcsolat.php">Kapcsolat</a>
+        </li>
+        <button class="btn btn-outline-success" onclick="window.location.href='student/login.php'">Bejelentkezés <i class="fa fa-user" aria-hidden="true"></i> </button>
+      
+    </div>
+  </div>
+</nav>
+
+
+<body>
