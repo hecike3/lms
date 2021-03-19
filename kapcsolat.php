157a65116a37acdca5e9ae3ff35c320453e76a5b
hecike3 <heczendorferpeter@gmail.com>
2021-03-19 18:04:49 +0100

    20210319 óra vége

--- kapcsolat.php
+++ kapcsolat.php
@@ -20,64 +20,61 @@ include "connection.php";
 <div class="container">
     <div class="row">
         <div class="col-lg-12">
-            <form  id="contact" action="https://formspree.io/f/xknpdzaa" method="POST">
-
+            <form id="my-form" action="https://formspree.io/f/xknpdzaa" method="POST">
                 <div class="row">
                     <div class="col">
+                        <div class="form-group mb-2">
+                            <div class="form-group mb-2">
+                                <label>Név:</label>
+                                <input type="text" name="name" class="form-control" />
 
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Teljes neved*</label>
-                            <input type="text" class="form-control" id="name"  required>
                             </div>
-                    </div>
-               
-                    <div class="col">
 
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">E-mail cím</label>
-                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
-                            <small id="emailHelp" class="form-text text-muted">Kisujj eskü soha senki nem látja majd az e-mail címed. Még mi sem. 🤞🏼</small>
+
                         </div>
-
-
                     </div>
+                    <div class="col">
+                        <label for="email">Email:</label>
+                        <input type="email" name="email" class="form-control" aria-describedby="emailHelp" />
+                        <small id="emailHelp" class="form-text text-muted">Ígérjük hogy nem mondjuk el senkinek
+                            🥐</small>
+                    </div>
+
                 </div>
 
                 <div class="row">
                     <div class="col">
+                        <div class="form-group mb-2">
+                            <div class="form-group mb-2">
+                                <label>Könyv:</label>
+                                <input type="text" name="konyv" class="form-control" />
 
-                        <div class="form-group">
-                            <label for="bookrecommended">Milyen könyvet szeretnél ajánlani?</label>
-                            <input type="text" class="form-control" id="book">
+                            </div>
+
+
                         </div>
                     </div>
-               
                     <div class="col">
-
-                        <div class="form-group">
-                            <label for="authername">Ki írta?</label>
-                            <input type="text" class="form-control" id="auther">
-                        </div>
+                        <label for="szerzo">Szerző:</label>
+                        <input type="text" name="szerzo" class="form-control" />
+                        
                     </div>
+
                 </div>
                 <div class="row">
                     <div class="col">
                         <div class="form-group">
-                            <label for="message">Üzeneted</label>
-                            <textarea class="form-control" id="message" rows="3"></textarea>
+                            <label for="uzenet">Üzenet</label>
+                            <textarea class="form-control"  name="uzenet" rows="3"></textarea>
                           </div>
                     </div>
                 </div>
-                    <div class="form-group form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1" required>
-                    <label class="form-check-label" for="exampleCheck1">Elfogadom az <a href="https://www.websiteplanet.com/hu/blog/tegye-adatvedelmi-iranyelveit-gdpr-kompatibilisse/" target="_blank">adatvédelmi irányelveket</a>.</label>
-                </div>
-                <button type="submit" class="btn btn-primary" id="send">Küldés</button>
-            </form>
 
-        <div class="alert alert-success mt-3" style = "display: none; " role="alert" id="status">
-            
-        </div>
+                <button id="my-form-button" class="btn btn-success" >Küldés</button>
+            </form>
+            <div class="alert mt-3" style="display: none;" role="alert" id="status">
+
+            </div>
 
         </div>
     </div>
