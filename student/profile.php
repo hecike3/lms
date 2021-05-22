a53b60c031a7cf6f4a88e7495a380ec269e2efb0
hecike3 <heczendorferpeter@gmail.com>
2021-05-22 18:03:18 +0200

    2021.05.22

--- student/profile.php
+++ student/profile.php
@@ -7,28 +7,59 @@
     if(isset($_POST["send"])){
         if(isset($_FILES["profilepic"])){
             $target_dir="profilepic/";
-            $target_file = $target_dir . basename($_FILES["profilepic"]["name"]);
+            $target_file = $target_dir .uniqid() . basename($_FILES["profilepic"]["name"]) ;
             $uploadOK = 1;
             $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
             $check = getimagesize($_FILES["profilepic"]["tmp_name"]);
-            if($check !== false){
-                echo "A fájl sikeresen fel lett töltve - " .$check["mime"];
-                
+
+            //csak jpg illetve png és gif 
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "gif" && $imageFileType != "jpeg"){
+                ?>
+                <script type="text/javascript">
+                  alert("A kép sajnos nem yo , csak JPG, JPEG, PNG és GIF használható!");
+                  window.location="profile.php";
+                  </script>
+                <?php
+                $uploadOK = 0;
+            }
+            if($_FILES["profilepic"]["size"] > 500000){
+                ?>
+                <script type="text/javascript">
+                  alert("A kép túl nagy! (Úristen very big )");
+                  window.location="profile.php";
+                  </script>
+                <?php
+            $uploadOK = 0;
+            }
+
+
+            if($check !== false && $uploadOK == 1){
+                //echo "A fájl sikeresen fel lett töltve - " .$check["mime"];
+                move_uploaded_file($_FILES["profilepic"]["tmp_name"],$target_file);
+                $sql = "UPDATE student_registration SET profilepic='$target_file' WHERE username='$_SESSION[username]'";
+                mysqli_query($link,$sql);
                 $uploadOK = 1;
+                ?>
+                <script type="text/javascript">
+                  alert("Sikeresen frissítetted az adataidat!");
+                  window.location="profile.php";
+                  </script>
+              <?php
             }else {
                 echo "nem yo a kép";
                 $uploadOK = 0;
             }
-        }else{
-             //  $sql = "UPDATE student_registration SET username='$_POST[felh]', firstname='$_POST[firstname]', lastname='$_POST[lastname]',email='$_POST[mail]', contact='$_POST[tel]' WHERE username='$_SESSION[username]'";
-             //  $_SESSION['username'] = $_POST['felh'];
-             //  mysqli_query($link,$sql);
         }
-
-
-
-            
-}            
+              $sql = "UPDATE student_registration SET username='$_POST[felh]', firstname='$_POST[firstname]', lastname='$_POST[lastname]',email='$_POST[mail]', contact='$_POST[tel]' WHERE username='$_SESSION[username]'";
+              $_SESSION['username'] = $_POST['felh'];
+              mysqli_query($link,$sql);
+            ?>
+              <script type="text/javascript">
+                alert("Sikeresen frissítetted az adataidat!");
+                window.location="profile.php";
+                </script>
+            <?php
+        }        
 ?>
  
  <!-- page content area main -->
@@ -60,7 +91,7 @@
 
                                 <form method="post" name="form1" enctype="multipart/form-data">
                                     <p>Felhasználó név : </p>
-                                    <input type="text" name="felh" value=" <?php echo "$res[username]"?>" class="form-control">
+                                    <input type="text" name="felh" value="<?php echo "$res[username]"?>" class="form-control">
                                     <p>Profilkép : </p>
                                     <input type="file" name="profilepic" class="form-control">
                                     <p>Vezetéknév : </p>
